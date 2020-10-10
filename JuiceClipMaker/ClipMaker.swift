81708de832a27fdf2ef9d9574bb7017731877966
Sergey Petrachkov <petrachkovsergey@gmail.com>
2020-10-10 13:50:58 +0300

    code cleaning + update animations so the text is coming from the left bottom corner

---
+++ JuiceClipMaker/ClipMaker.swift
@@ -0,0 +1,275 @@
+//
+//  ClipMaker.swift
+//  JuiceClipMaker
+//
+//  Created by sergey on 10.10.2020.
+//
+
+import UIKit
+import AVFoundation
+
+class ClipMaker {
+  enum Errors: Swift.Error {
+    case assetError
+    case audioError
+    case exportSessionCreationError
+    case exportError
+  }
+  func decorateVideoWithEffects(videoURL: URL, addingText text: String, onComplete: @escaping (Result<URL, Error>) -> Void) {
+    print(videoURL)
+    let asset = AVURLAsset(url: videoURL)
+    let composition = AVMutableComposition()
+
+    let compTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)
+    guard let compositionTrack = compTrack,
+          let assetTrack = asset.tracks(withMediaType: .video).first else {
+      onComplete(.failure(Errors.assetError))
+      return
+    }
+
+    do {
+      let timeRange = CMTimeRange(start: .zero, duration: asset.duration)
+      try compositionTrack.insertTimeRange(timeRange, of: assetTrack, at: .zero)
+
+      let audioComp = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+      if let audioAssetTrack = asset.tracks(withMediaType: .audio).first,
+         let compositionAudioTrack = audioComp {
+
+        try compositionAudioTrack.insertTimeRange(
+          timeRange,
+          of: audioAssetTrack,
+          at: .zero
+        )
+      }
+    } catch {
+      onComplete(.failure(Errors.audioError))
+      return
+    }
+
+    compositionTrack.preferredTransform = assetTrack.preferredTransform
+
+    let videoInfo = self.orientation(from: assetTrack.preferredTransform)
+
+    let videoSize: CGSize
+    if videoInfo.isPortrait {
+      videoSize = CGSize(width: assetTrack.naturalSize.height, height: assetTrack.naturalSize.width)
+    } else {
+      videoSize = assetTrack.naturalSize
+    }
+
+    let backgroundLayer = CALayer()
+    backgroundLayer.frame = CGRect(origin: .zero, size: videoSize)
+    let videoLayer = CALayer()
+    videoLayer.frame = CGRect(origin: .zero, size: videoSize)
+    let overlayLayer = CALayer()
+    overlayLayer.frame = CGRect(origin: .zero, size: videoSize)
+
+    let color = UIColor(red: 85.0/255, green: 153.0/255, blue: 236.0/255, alpha: 1)
+    backgroundLayer.backgroundColor = color.cgColor
+    videoLayer.frame = CGRect(
+      x: 20,
+      y: 20,
+      width: videoSize.width - 40,
+      height: videoSize.height - 40)
+
+    backgroundLayer.contents = UIImage.from(color: color).cgImage
+    backgroundLayer.contentsGravity = .resizeAspectFill
+
+    self.add(
+      text: text,
+      backgroundColor: color,
+      to: overlayLayer,
+      videoSize: videoSize
+    )
+
+    let outputLayer = CALayer()
+    outputLayer.frame = CGRect(origin: .zero, size: videoSize)
+    outputLayer.addSublayer(backgroundLayer)
+    outputLayer.addSublayer(videoLayer)
+    outputLayer.addSublayer(overlayLayer)
+
+    let videoComposition = AVMutableVideoComposition()
+    videoComposition.renderSize = videoSize
+    videoComposition.frameDuration = CMTime(value: 1, timescale: 30)
+    videoComposition.animationTool = AVVideoCompositionCoreAnimationTool(
+      postProcessingAsVideoLayer: videoLayer,
+      in: outputLayer
+    )
+
+    let instruction = AVMutableVideoCompositionInstruction()
+
+    instruction.timeRange = CMTimeRange(
+      start: .zero,
+      duration: composition.duration
+    )
+
+    videoComposition.instructions = [instruction]
+
+    let layerInstruction = compositionLayerInstruction(
+      for: compositionTrack,
+      assetTrack: assetTrack
+    )
+    instruction.layerInstructions = [layerInstruction]
+
+    guard let export = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetHighestQuality) else {
+      onComplete(.failure(Errors.exportSessionCreationError))
+      return
+    }
+
+    let tempDirectory = NSTemporaryDirectory()
+    let tempVideoName = UUID().uuidString
+    let exportURL = URL(fileURLWithPath: tempDirectory.appending(tempVideoName).appending(".mp4"))
+    export.shouldOptimizeForNetworkUse = true
+    export.videoComposition = videoComposition
+
+    export.outputFileType = .mp4
+    export.outputURL = exportURL
+
+    export.exportAsynchronously {
+      DispatchQueue.main.async {
+        switch export.status {
+        case .completed:
+          onComplete(.success(exportURL))
+        default:
+          print(export.error ?? "unknown error")
+          onComplete(.failure(export.error ?? Errors.exportError))
+          break
+        }
+      }
+    }
+  }
+
+  private func addImage(to layer: CALayer, videoSize: CGSize) {
+    let image = UIImage(named: "overlay")!
+    let imageLayer = CALayer()
+
+    let aspect: CGFloat = image.size.width / image.size.height
+    let width = videoSize.width
+    let height = width / aspect
+    imageLayer.frame = CGRect(
+      x: 0,
+      y: -height * 0.15,
+      width: width,
+      height: height)
+
+    imageLayer.contents = image.cgImage
+    layer.addSublayer(imageLayer)
+  }
+
+  private func add(text: String, backgroundColor: UIColor, to layer: CALayer, videoSize: CGSize) {
+    let attributedText = NSAttributedString(
+      string: text,
+      attributes: [
+        .font: UIFont.systemFont(ofSize: 35),
+        .foregroundColor: UIColor.white,
+        .strokeColor: UIColor.white,
+        .strokeWidth: -3,
+      ]
+    )
+
+    let textRect = attributedText.boundingRect(
+      with: videoSize,
+      options: .usesFontLeading,
+      context: nil
+    )
+
+    let textLayer = CATextLayer()
+    textLayer.string = attributedText
+    textLayer.backgroundColor = backgroundColor.cgColor
+    textLayer.alignmentMode = .center
+    textLayer.cornerRadius = 4
+
+    textLayer.frame = CGRect(
+      x: -textRect.width,
+      y: textRect.height,
+      width: textRect.width + 24,
+      height: textRect.height)
+    textLayer.displayIfNeeded()
+
+
+    let frameAnimation = CABasicAnimation(keyPath: "position.x")
+    frameAnimation.fromValue = -textRect.width
+    frameAnimation.toValue = textRect.width/2 + 40
+    frameAnimation.duration = 0.8
+    frameAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+    frameAnimation.beginTime = AVCoreAnimationBeginTimeAtZero
+    frameAnimation.autoreverses = false
+    frameAnimation.isRemovedOnCompletion = true
+    textLayer.position.x = textRect.width/2 + 40
+
+    textLayer.add(frameAnimation, forKey: nil)
+
+    layer.addSublayer(textLayer)
+    textLayer.setNeedsDisplay()
+  }
+
+  private func orientation(from transform: CGAffineTransform) -> (orientation: UIImage.Orientation, isPortrait: Bool) {
+    var assetOrientation = UIImage.Orientation.up
+    var isPortrait = false
+    if transform.a == 0 && transform.b == 1.0 && transform.c == -1.0 && transform.d == 0 {
+      assetOrientation = .right
+      isPortrait = true
+    } else if transform.a == 0 && transform.b == -1.0 && transform.c == 1.0 && transform.d == 0 {
+      assetOrientation = .left
+      isPortrait = true
+    } else if transform.a == 1.0 && transform.b == 0 && transform.c == 0 && transform.d == 1.0 {
+      assetOrientation = .up
+    } else if transform.a == -1.0 && transform.b == 0 && transform.c == 0 && transform.d == -1.0 {
+      assetOrientation = .down
+    }
+
+    return (assetOrientation, isPortrait)
+  }
+
+  private func compositionLayerInstruction(
+    for track: AVCompositionTrack,
+    assetTrack: AVAssetTrack) -> AVMutableVideoCompositionLayerInstruction {
+
+    let instruction = AVMutableVideoCompositionLayerInstruction(assetTrack: track)
+    let transform = assetTrack.preferredTransform
+
+    instruction.setTransform(transform, at: .zero)
+
+    return instruction
+  }
+
+  private func addConfetti(to layer: CALayer) {
+    let images: [UIImage] = (0...5).map { UIImage(named: "confetti\($0)")! }
+    let colors: [UIColor] = [.systemGreen, .systemRed, .systemBlue, .systemPink, .systemOrange, .systemPurple, .systemYellow]
+    let cells: [CAEmitterCell] = (0...16).map { _ in
+      let cell = CAEmitterCell()
+      cell.contents = images.randomElement()?.cgImage
+      cell.birthRate = 3
+      cell.lifetime = 12
+      cell.lifetimeRange = 0
+      cell.velocity = CGFloat.random(in: 100...200)
+      cell.velocityRange = 0
+      cell.emissionLongitude = 0
+      cell.emissionRange = 0.8
+      cell.spin = 4
+      cell.color = colors.randomElement()?.cgColor
+      cell.scale = CGFloat.random(in: 0.2...0.8)
+      return cell
+    }
+
+    let emitter = CAEmitterLayer()
+    emitter.emitterPosition = CGPoint(x: layer.frame.size.width / 2, y: layer.frame.size.height + 5)
+    emitter.emitterShape = .line
+    emitter.emitterSize = CGSize(width: layer.frame.size.width, height: 2)
+    emitter.emitterCells = cells
+
+    layer.addSublayer(emitter)
+  }
+}
+
+extension UIImage {
+  static func from(color: UIColor, rect: CGRect = CGRect(x: 0, y: 0, width: 1, height: 1)) -> UIImage {
+    UIGraphicsBeginImageContext(rect.size)
+    let context = UIGraphicsGetCurrentContext()
+    context!.setFillColor(color.cgColor)
+    context!.fill(rect)
+    let img = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return img!
+  }
+}
